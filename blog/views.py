b2c11c3d3f843460e99212175c6a6b70f0642efc
YaroslavVasylechko <yaroslav.vasylechko@gmail.com>
2021-08-08 00:52:20 +0200

    added pagination

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from .models import *
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic Block Design'
+        return context
 
 
 def index(request):
@@ -7,4 +20,7 @@ def index(request):
 
 
 def get_category(request, slug):
+    return render(request, 'blog/category.html')
+
+def get_post(request, slug):
     return render(request, 'blog/category.html')
